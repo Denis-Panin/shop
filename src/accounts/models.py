a063cf7e7262a05b162aa7345edb80cb5e18d206
Denis Panin <deniswork2021@ukr.net>
2022-12-25 16:43:44 +0100

    added model "Seller", added urls.py in accounts, added models to admin

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,11 +3,32 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
+
+    class Meta:
+        verbose_name = "Користувач"
+        verbose_name_plural = "Користувачі"
+
     first_name = models.CharField("Ім'я", max_length=32)
     last_name = models.CharField("Прізвище", max_length=32)
     email = models.EmailField("Email")
     age = models.IntegerField("Вік")
     phone_number = PhoneNumberField("Номер телефону", null=True, blank=True, unique=True)
     
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Seller(models.Model):
+
+    class Meta:
+        verbose_name = "Продавець"
+        verbose_name_plural = "Продавці"
+
+    first_name = models.CharField("Ім'я", max_length=32)
+    last_name = models.CharField("Прізвище", max_length=32)
+    age = models.IntegerField("Вік")
+    phone_number = PhoneNumberField("Номер телефону", blank=True, null=True, unique=True)
+    role = models.CharField("Роль", max_length=32)
+    experience = models.IntegerField("Стаж роботи")
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
-# Create your views here.
+def users(request):
+    return HttpResponse('hello Users')
